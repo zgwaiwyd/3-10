ae87ae6c1da42870059334ba9aa7f6beaf4707bd
zgwaiwyd <99521061+zgwaiwyd@users.noreply.github.com>
2022-03-10 22:25:34 +0800

    Add files via upload

---
+++ 3-10.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<time.h>
+#include<math.h>
+#include<stdlib.h>
+int main()
+#include  <stdio.h>
+#include  <stdlib.h>
+void fun(int* a, int n)
+{
+    int  i;
+    for (i = 0; i < n; i++)
+    {
+        /**********found**********/
+        if (___1___ == 0)
+            /**********found**********/
+            printf("___2___");
+        /**********found**********/
+        printf("%d  ", ___3___);
+    }
+}
+void main()
+{
+    int  a[100] = { 0 }, i, n;
+    n = 22;
+    for (i = 0; i < n; i++) a[i] = rand() % 21;
+    fun(a, n);
+    printf("\n");
+}
+
